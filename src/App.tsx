46c30dc5ec13561e15f220c215bcf23608f5e86f
raffyalbar30 <geminglol09@gmail.com>
2024-07-08 09:29:43 +0700

    set up uploader project

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Testing from './Components/testing'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+          <p>heloo world</p>
+          <Testing></Testing>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/testing.tsx
@@ -0,0 +1,10 @@
+
+const Testing = () => {
+    return (
+        <div>
+            <p className='text-blue-300'>heloo raff</p> 
+        </div>
+    );
+}
+
+export default Testing;
